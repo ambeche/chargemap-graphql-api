e6bdc668f7601442efc4f8639c98642304feddb5
tamanjic <tamanji.che@metropolia.fi>
2021-04-14 02:03:22 +0300

    passport-local + jwt strategy

---
+++ auth/auth.js
@@ -0,0 +1,46 @@
+'use strict';
+import env from 'dotenv';
+import jwt from 'jsonwebtoken';
+import passport from './passport.js';
+
+env.config();
+
+
+const login = (req, res) => {
+  return new Promise((resolve, reject) => {
+    passport.authenticate('local', {session: false},
+        async (err, user, info) => {
+          try {
+            if (err || !user) {
+              reject(info.message);
+            }
+            req.login(user, {session: false}, async (err) => {
+              if (err) {
+                reject(err);
+              }
+    
+              const token = jwt.sign(user, process.env.SECRET); // jwt encoding
+              resolve({user, token});
+            });
+          } catch (e) {
+            reject({message: e.message});
+          }
+        })(req, res);
+  });
+};
+
+const checkAuth = (req, res) => {
+  return new Promise((resolve, reject) => {
+    passport.authenticate('jwt', (err, user) => {
+      if (err || !user) {
+        resolve(false);
+      }
+      resolve(user);
+    })(req, res);
+  });
+};
+
+export {
+  login,
+  checkAuth,
+};
---
+++ auth/passport.js
@@ -0,0 +1,62 @@
+'use strict';
+import passport from 'passport';
+import {Strategy} from 'passport-local';
+import passportJWT from "passport-jwt";
+import bcrypt from 'bcrypt';
+import User from '../models/user.js';
+import env from 'dotenv';
+
+const ExtractJWT = passportJWT.ExtractJwt;
+const JWTStrategy   = passportJWT.Strategy;
+env.config();
+
+
+// local strategy for username password login
+passport.use(new Strategy(
+    async (username, password, done) => {
+  
+      try {
+        const user = await User.findone({username});
+        console.log('Local strategy', user);
+        if (user === null ) {
+          return done(null, false, {message: 'invalid credentials'});
+        }
+
+        const validateUser = await bcrypt.compare(password, user.password);
+
+        if (!validateUser) {
+          return done(null, false, {message: 'invalid credentials!'});
+        }
+
+        const userWithoutPass = {...user}
+        delete userWithoutPass.password // delete user's password for security
+
+        return done(null, userWithoutPass, {message: 'Logged In Successfully'}); 
+      } catch (err) {
+        return done(err);
+      }
+    }));
+
+// JWT strategy for handling bearer token
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey   : process.env.SECRET
+},
+ async (jwtPayload, done) => {
+  try {
+    const user = await User.findById(jwtPayload._id);
+    console.log('jwt strategy', jwtPayload);
+    if (user === null) {
+      return done(null, false);
+    }
+
+    return done(null, user); 
+  } catch (err) {
+    return done(err);
+  }
+}
+));
+
+
+export default passport;
